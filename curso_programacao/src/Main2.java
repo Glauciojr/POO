bf5371a3cb4d4a5a3407ee5a41e695725f5f2bb0
Glauciojr <glauciosouzajrs@gmail.com>
2022-03-01 22:32:02 -0300

    (01/03) Inicio

---
+++ curso_programacao/src/Main2.java
@@ -0,0 +1,63 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+		/* Scanner sc = new Scanner(System.in);
+		 
+		 String x;
+		 x = sc.next();
+		 System.out.println("Você digitou: " + x);
+		 
+		 sc.close();
+		 
+		 
+		
+         Scanner sc = new Scanner(System.in);
+		 
+		 int x;
+		 x = sc.nextInt();
+		 System.out.println("Você digitou: " + x);
+		 
+		 sc.close();
+		 
+		 */
+		 Locale.setDefault(Locale.US);
+		 /*Scanner sc = new Scanner(System.in);
+		 
+		 double x;
+		 x = sc.nextDouble();
+		 System.out.println("Você digitou: " + x);
+		 
+		 sc.close();
+		 
+		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 char x;
+		 x = sc.next().charAt(0);
+		 System.out.println("Você digitou: " + x);
+		 
+		 sc.close();
+		 
+		 */
+		 
+		 Scanner sc = new Scanner(System.in);
+		 
+		 String x;
+		 int y;
+		 double z;
+		 x = sc.next();
+		 y = sc.nextInt();
+		 z = sc.nextDouble();
+		 System.out.println("Dados digitados: ");
+		 System.out.println(x);
+		 System.out.println(y);
+		 System.out.println(z);
+		 
+		 sc.close();
+	}
+
+}
